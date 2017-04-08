2cdad70a60c616c01a91a5d821dccaa18b774a2f
Anish Gulati <anish.gulati@research.iiit.ac.in>
2017-04-08 23:17:05 +0000

    Replace controllers.py

--- src/app/seat/controllers.py
+++ src/app/seat/controllers.py
@@ -6,8 +6,25 @@ from sqlalchemy import *
 mod_seat = Blueprint('seat', __name__, url_prefix='/api')
 
 
-@mod_seat.route('/seat', methods=['GET'])
-def display_seat():
-    seats = Seat.query.all()
-    return jsonify(success=True),200
+@mod_seat.route('/seat/set', methods=['POST'])
+def set_seat_cost():
+	platinum =  int(request.form['platinum'])
+	gold = int(request.form['gold'])
+	silver = int(request.form['silver'])
+	for i in range(65,72):
+		for j in range(1,16):
+			s = Seat(chr(i),j,silver)
+			db.session.add(s)
+	for i in range(72,77):
+		for j in range(1,16):
+			s = Seat(chr(i),j,gold)
+			db.session.add(s)
+
+	for i in range(77,80):
+		for j in range(1,16):
+			s = Seat(chr(i),j,platinum)
+			db.session.add(s)
+	db.session.commit()
+	return jsonify(success=True),200
+
 
