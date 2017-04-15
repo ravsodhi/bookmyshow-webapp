066b88a7638945fb2ad197da2d8b17237015ae1e
Kunalgarg2100 <kunalgarg2100@gmail.com>
2017-04-15 13:27:42 +0000

    Update config.py

--- src/config.py
+++ src/config.py
@@ -47,7 +47,7 @@ DATABASE_CONNECT_OPTIONS = {}
 THREADS_PER_PAGE = 2
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
-CSRF_ENABLED     = False
+CSRF_ENABLED     = True
 
 # Use a secure, unique and absolutely secret key for
 # signing the data.
